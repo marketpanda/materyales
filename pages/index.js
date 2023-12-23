c4db3ad726bcf997c427afaf588abd4fe3a4451f
marketpanda <91251429+marketpanda@users.noreply.github.com>
2023-12-23 20:04:16 +0800

    getstaticpath and getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,49 @@
-export default function Recipes() {
+import { createClient } from "contentful"
+import { useEffect, useState } from "react"
+import RecipeCard from "../components/RecipeCard"
+
+export async function getRecipes() {
+  const client  = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY
+    // space: process.env.CONTENTFUL_SPACE_ID,
+    // accessToken: process.env.CONTENTFUL_ACCESS_KEY
+  })
+
+  if (client) {
+    console.log('success')
+  }
+
+  const res = await client.getEntries({ content_type: 'recipe', include: 2})
+  console.log(res.items)
+
+  return res.items 
+}   
+
+export default function Recipes() { 
+    
+  const [theRecipes, setTheRecipes] = useState([])
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      const recipes = await getRecipes()
+      setTheRecipes(recipes)
+      console.log(recipes)
+    }
+
+    fetchRecipes()
+    
+  }, [])
+   
+
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className="grid grid-cols-2 gap-x-5 gap-y-15">
+      {
+        theRecipes.map(recipe => (  
+          <RecipeCard key={recipe.sys.id} recipe={recipe} />
+        ))
+      }
     </div>
   )
 }
+
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,44 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful"
+ 
+const client  = createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY
+})
+
+ 
+
+export const getStaticPaths = async() => {
+  const res = await client.getEntries({
+    content_type: 'recipe',
+
+  })
+
+  const paths =res.items.map(item => {
+    return {
+      params: {
+        slug: item.fields.slug
+      }
+    }
+  })
+  return {
+    paths,
+    fallback: false
+
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items} = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug
+  })
+  return {
+    props: { recipe: items[0]}
+  }
+}
+
+export default function RecipeDetails({ recipe }) {   
+  console.log(recipe)
   return (
     <div>
       Recipe Details
