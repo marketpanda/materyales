0901f3795b0c78ec8afab89cbb864e0f3dbdc5cc
marketpanda <91251429+marketpanda@users.noreply.github.com>
2024-01-23 20:21:38 +0800

    site meta

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Materyales',
+  author: 'architect | marketpanda',
+  headerTitle: 'Materyales',
+  description: 'Online easy estimator',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://materyales.com/',
+  siteRepo: 'https://github.com/marketpanda/materyales3',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
