eeb22c0c55e9d127955cd0ed394e67057370fd7b
marketpanda <91251429+marketpanda@users.noreply.github.com>
2024-03-12 07:53:11 +0800

    getBuildingLaw made local

--- src/app/building-laws/[slug]/page.tsx
+++ src/app/building-laws/[slug]/page.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import { createClient } from 'contentful' 
+  import React from 'react'
+  import { createClient } from 'contentful' 
 
-const client = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string
-})
-
-export const generateStaticParams = async() => {
-  const res = await client.getEntries({ content_type: 'recipe'})
-
-  return res.items.map((item) => ({
-    slug: item.fields.slug
-  }))
-}
-
- 
-
-export const getBuildingLaw = async(theSlug: string) => {
-  const { items } = await client.getEntries({
-    content_type: 'recipe',
-    'fields.slug': theSlug
+  const client = createClient({
+      space: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
+      accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string
   })
 
-  return  items[0] 
-}
+  export const generateStaticParams = async() => {
+    const res = await client.getEntries({ content_type: 'recipe'})
 
-const page = ({ params }: { params: { slug: string }}) => { 
-  const buildingLaw = getBuildingLaw(params.slug)
-  console.log(buildingLaw)
-  return (
-    <div>a</div> 
-  )
-}
+    return res.items.map((item) => ({
+      slug: item.fields.slug
+    }))
+  }
 
-export default page
+  
+
+  const getBuildingLaw = async(theSlug: string) => {
+    const { items } = await client.getEntries({
+      content_type: 'recipe',
+      'fields.slug': theSlug
+    })
+
+    return  items[0] 
+  }
+
+  const page = ({ params }: { params: { slug: string }}) => { 
+    const buildingLaw = getBuildingLaw(params.slug)
+    console.log(buildingLaw)
+    return (
+      <div>a</div> 
+    )
+  }
+
+  export default page
