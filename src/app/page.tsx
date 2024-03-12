964663cf14f2862b71acbfc8f345395461805d29
marketpanda <91251429+marketpanda@users.noreply.github.com>
2024-03-12 20:09:58 +0800

    basic calculation and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,15 @@ export default function Home() {
   const [width, setWidth] = useState<number |null>(null)
   const [length, setLength] = useState<number |null>(null)
 
-  const [area, setArea] = useState<number |null>(null)
+  const [area, setArea] = useState<number | null | string>(null)
 
 
   const displayArea = () => {
      if (typeof width === 'number' && typeof length === 'number') {
       const area2: number = width * length;
-      setArea(area2);
+
+      const cleanDecimals: string = area2.toFixed(2)
+      setArea(cleanDecimals);
     }
   }
 
@@ -25,66 +27,106 @@ export default function Home() {
     }
   }, [width, length])
 
+  useEffect(() => {
+    console.log(area)
+    return () => {
+
+    }
+  }, [area])
+
   const handleLengthChange = (e:any) => {
-    setLength(parseFloat(e?.target.value))
-    
+    setLength(parseFloat(e?.target.value))    
   }
+  
   const handleWidthChange = (e:any) => {
     setWidth(parseFloat(e?.target.value))
-     
+  }
+
+  const handleDirectArea = () => {
+    setWidth(null)
+    setLength(null)
+    setArea(null)
+  }
+
+  const handleAreaChange = (e:any) => {
+    setArea(parseFloat(e.target.value))
   }
 
    
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-[640px] border-4 rounded ">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="w-full sm:w-[640px] border-4 rounded p-2 flex flex-col">
       
-        <form className="flex flex-col gap-2 p-2">
-          <div className="flex gap-2 items-center">
-            <div className="w-12 text-right">
+        <form className="flex flex-col gap-2">
+          <div className="flex flex-col sm:flex-row gap-2 items-center">
+            <div className="w-full sm:w-20 px-2 text-left sm:text-right font-bold">
               Length
             </div>
-            <input
-              value={!length ? '' : length}
-              onChange={handleLengthChange}
-              placeholder="Type a length"
-              type="number"
-              name="length"
-              className="p-2 flex-grow outline-none font-bold text-purple-800 text-2xl text-right" />
-            <div className="w-12 text-left">
-              m
+            <div className="flex w-full items-center bg-white">
+              <input
+                value={!length ? '' : length}
+                onChange={handleLengthChange}
+                placeholder="Type a length"
+                type="number"
+                name="length"
+                className="p-2 w-full outline-none font-bold text-purple-800 text-2xl text-right" />
+              <div className="w-16 text-left text-2xl font-bold text-purple-800">
+                m
+              </div>
             </div>
           </div>
-          <div className="flex gap-2 items-center">
-            <div className="w-12 text-right">
+          <div className="flex flex-col sm:flex-row gap-2 items-center">
+            <div className="w-full sm:w-20 px-2 text-left sm:text-right font-bold">
               Width
             </div>
-            <input 
-              value={!width ? '' : width}
-              onChange={handleWidthChange}
-              placeholder="Type a width"
-              type="number"
-              name="length"
-              className="p-2 flex-grow outline-none font-bold text-purple-800 text-2xl text-right" />
-            <div className="w-12 text-left">
-              m
+            <div className="flex w-full items-center bg-white">
+              <input
+                value={!width ? '' : width}
+                onChange={handleWidthChange}
+                placeholder="Type a width"
+                type="number"
+                name="length"
+                className="p-2 w-full outline-none font-bold text-purple-800 text-2xl text-right" />
+              <div className="w-16 text-left text-2xl font-bold text-purple-800">
+                m
+              </div>
             </div>
           </div>
-          <div className="flex gap-2 items-center">
-            <div className="w-12 text-right">
+          <div className="flex flex-col sm:flex-row gap-2 items-center">
+            <div className="w-full sm:w-20 px-2 text-left sm:text-right font-bold">
               Area
             </div>
-            <input
-              value={area !== null ? area : ''}
-              type="number" name="length" className="p-2 flex-grow outline-none font-bold text-purple-800 text-2xl text-right" />
-              <div className="w-12 text-left">
-              sqm
+            <div className="flex w-full items-center bg-white">
+              <input
+                value={!area ? '' : area}
+                onChange={handleAreaChange}
+                onClick={handleDirectArea}
+                placeholder="Type an area or generate"
+                type="number"
+                name="length"
+                className="p-2 w-full outline-none font-bold text-purple-800 text-2xl text-right" />
+              <div className="w-16 text-left text-2xl font-bold text-purple-800">
+                sqm
+              </div>
             </div>
+          </div> 
+        </form>
+        {/* <div className="mt-2 flex flex-col w-full">
+          
+          <div className="w-full flex justify-between">
+            <span className="w-1/2">
+              tiles
+            </span>
+            <span className="w-1/2">
+              100pcs
+            </span>
+            
           </div>
           
-          
-        </form>
+        </div> */}
       </div>
+
+     
     </main>
   );
 }
