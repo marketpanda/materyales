85ce722ac3bcc1d9a1714096c616925ae6ebf86c
marketpanda <91251429+marketpanda@users.noreply.github.com>
2024-05-04 10:15:56 +0800

    type fix

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -130,7 +130,7 @@ const Navbar:React.FC = () => {
   );
 };
 
-const ListItem = React.forwardRef<HTMLLIElement, ListItemProps >(({ className, children, title, ...props }, forwardedRef) => (
+const ListItem = React.forwardRef<any, ListItemProps >(({ className, children, title, ...props }, forwardedRef) => (
   <li>
     <NavigationMenu.Link asChild>
       <a
@@ -147,5 +147,8 @@ const ListItem = React.forwardRef<HTMLLIElement, ListItemProps >(({ className, c
     </NavigationMenu.Link>
   </li>
 ));
+
+//for typing error
+ListItem.displayName = 'ListItem'
 
 export default Navbar;
