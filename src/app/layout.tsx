d3d4985950a8a599fdf789ae626dd0ec624f772e
marketpanda <jrdollesin@gmail.com>
2025-05-20 07:51:33 +0800

    retry clarity ms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+         
         <body className={inter.className}>
           <Theme accentColor="crimson"> 
             {children}
