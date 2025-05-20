369d10ce355d060c4c8e7d8f30e88ac5c6af0144
marketpanda <jrdollesin@gmail.com>
2025-05-20 08:05:20 +0800

    retry ms clarity via script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <head>
+        {/* Microsoft Clarity Script */}
+        {process.env.NODE_ENV === 'production' && (
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rkvc36hqm0");`,
+          }}
+        />
+        )}
+      </head>
          
         <body className={inter.className}>
           <Theme accentColor="crimson"> 
